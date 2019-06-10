a63a4dbdc25d6e258383c96d44703e3473af4232
Hossein Margani <margani.h@gmail.com>
2019-06-10 17:27:50 +0100

    Improved the desktop initially
    (cherry picked from commit 712d71ed86fc61bd21a3878ebf664e4b61841677)

--- Desktop/src/renderer/App.vue
+++ Desktop/src/renderer/App.vue
@@ -1,15 +1,67 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
+    <main>
+      <div class="row">
+        <div class="col-sm-4">
+          <span class="title">Welcome to Octopus Search!</span>
+          <a href="#">Search variables</a>
+          <a href="#">Search projects</a>
+          <a href="#">Search targets</a>
+          <router-link to="/settings">Settings</router-link>
+          <a href="#">About</a>
+          <system-information></system-information>
+        </div>
+        <div class="col-sm-8">
+          <router-view></router-view>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'octopussearch-desktop'
-  }
+export default {
+  name: "octopussearch-desktop"
+};
 </script>
 
 <style>
-  /* CSS */
+@import "~bootstrap/dist/css/bootstrap.css";
+@import "~bootstrap-vue/dist/bootstrap-vue.css";
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Source Sans Pro", sans-serif;
+}
+
+#app {
+  background: radial-gradient(
+    ellipse at top left,
+    rgba(255, 255, 255, 1) 40%,
+    rgba(229, 229, 229, 0.9) 100%
+  );
+  height: 100vh;
+  padding: 60px 80px;
+  width: 100vw;
+}
+
+#logo {
+  height: auto;
+  margin-bottom: 20px;
+  width: 420px;
+}
+
+.welcome {
+  color: #555;
+  font-size: 23px;
+  margin-bottom: 10px;
+}
+/* CSS */
 </style>
