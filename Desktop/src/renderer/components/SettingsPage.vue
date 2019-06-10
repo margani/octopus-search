712d71ed86fc61bd21a3878ebf664e4b61841677
Hossein Margani <margani.h@gmail.com>
2019-06-10 17:27:50 +0100

    Improved the desktop initially

---
+++ Desktop/src/renderer/components/SettingsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Enter the city name to check current weather in it</p>
+    <section class="weather-input">
+      <input class="form-control" type="text">
+      <button class="btn btn-primary">Check</button>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'settings-page',
+  components: { },
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
